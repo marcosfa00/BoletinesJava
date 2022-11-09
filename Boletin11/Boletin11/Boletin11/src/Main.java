6e9d71e5ef114b6083bb344c33c9fdd99c3efd98
Marcos F. Avendaño <marcosfavendano@gmail.com>
2022-11-09 14:04:58 +0100

    subo lo ultimo

--- Boletin11/Boletin11/Boletin11/src/Main.java
+++ Boletin11/Boletin11/Boletin11/src/Main.java
@@ -1,5 +1,35 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ADIVINA EL NÚMERO");
+        Metodos metodo = new Metodos();
+        int j = Integer.parseInt(JOptionPane.showInputDialog("INTRODUCE NUMERO DE JUGADORES 1 O 2?"));
+        int random = (int) Math.random() * 51 +1;
+        int num;
+        if (j == 1){
+            num = random;
+            metodo.setNum(num);
+        }else {
+            num = Integer.parseInt(JOptionPane.showInputDialog("INTRODUCE EL NÚMERO QUE SE VA HA ADIVINAR"));
+            metodo.validarNum(num);
+            num = metodo.getNum();
+        }
+        int intentos = Integer.parseInt(JOptionPane.showInputDialog("INTRODUCE UN NÚMERO MÁXIMO DE INTENTOS"));
+        metodo.validarIntentos(intentos);
+        intentos = metodo.getIntentos();
+        int i = 0;
+        do {
+
+            int adv_num = Integer.parseInt(JOptionPane.showInputDialog("ADIVINA EL NUMERO"));
+          metodo.primeraPista(adv_num);
+          if (metodo.getAdivinar()){
+              i += intentos;
+          }
+
+
+        }while (i < intentos);
+
+
     }
 }
--- Boletin11/Boletin11/Boletin11/src/Metodos.java
+++ Boletin11/Boletin11/Boletin11/src/Metodos.java
@@ -1,3 +1,58 @@
+import javax.swing.*;
+
 public class Metodos {
+    private  boolean adivinar = false;
+    private int num;
+    private int intentos;
+    public boolean getAdivinar(){
+        return this.adivinar;
+    }
+    public int getNum(){
+        return this.num;
+    }
+    public void setNum(int num){
+        this.num = num;
+    }
+    public int getIntentos(){
+        return this.intentos;
+    }
+    public void primeraPista(int adv_num){
+
+        if(num > adv_num){
+            JOptionPane.showMessageDialog(null,"EL NUMERO ES MAYOR");
+        }else if(num < adv_num) {
+            JOptionPane.showMessageDialog(null,"EL NUMERO ES MENOR");
+        }else {
+            JOptionPane.showMessageDialog(null,"ADIVINASTE");
+            this.adivinar = true;
+        }
+
+    }
+
+    public void validarNum(int num){
+        if (num < 1 || num > 50){
+            do {
+                JOptionPane.showMessageDialog(null,"¡¡COMO MINIMO 1 Y MÁXIMO 50!!");
+                this.num = Integer.parseInt(JOptionPane.showInputDialog("INTRODUCE UN NÚMERO DEL 1 AL 50"));
+            }while (this.num < 1 || this.num > 50);
+        }else {
+            this.num = num;
+        }
+
+
+    }
+    public void validarIntentos(int intentos){
+        if (intentos < 1 || intentos > 15){
+            do {
+                JOptionPane.showMessageDialog(null,"¡¡ALAAA A DONDE VÁS, CÓMO MÁXIMO 15 INTENTOS!!");
+                this.intentos = Integer.parseInt(JOptionPane.showInputDialog("INTRODUCE EL NÚMERO MÁXIMO DE INTENTOS"));
+            }while (this.intentos < 1 || this.intentos > 15);
+        }else {
+            this.intentos = intentos;
+        }
+    }
+
+
+
 
 }
