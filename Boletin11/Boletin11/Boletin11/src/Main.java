416581d675e5285ff7076e1c83fc304bb40a3d76
Marcos F. Avendaño <marcosfavendano@gmail.com>
2022-11-22 08:40:56 +0100

    comenzamos con paquetes

--- Boletin11/Boletin11/Boletin11/src/Main.java
+++ Boletin11/Boletin11/Boletin11/src/Main.java
@@ -5,26 +5,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //Comento esto para ver si funciona
+        String str_n_players = "INTRODUCE NÚMERO DE JUGADORES 1 O 2?";
+        String str_numero = "INTRODUCE EL NÚMERO QUE SE VA HA ADIVINAR";
+        String str_n_max_intentos = "INTRODUCE UN NÚMERO MÁXIMO DE INTENTOS";
+        String str_adv_num = "ADIVINA EL NÚMERO, TE QUEDAN ";
+        String str_intentos = "INTENTOS";
+        String str_fallaste= "OOOH!! HAS FALLADO, EL NÚMERO ERA: ";
         Metodos metodo = new Metodos();
-        int j = Integer.parseInt(JOptionPane.showInputDialog("INTRODUCE NÚMERO DE JUGADORES 1 O 2?"));
+        int j = Integer.parseInt(JOptionPane.showInputDialog(str_n_players));
         double random =  Math.random() * 50 +1;
-        int num;
+        int numero_a_adivinar;
         if (j == 1){
-            num =(int) random;
-            metodo.setNum(num);
+            numero_a_adivinar =(int) random;
+            metodo.setNum(numero_a_adivinar);
         }else {
-            num = Integer.parseInt(JOptionPane.showInputDialog(null,"INTRODUCE EL NÚMERO QUE SE VA HA ADIVINAR"));
-            metodo.validarNum(num);
-            num = metodo.getNum();
+            numero_a_adivinar = Integer.parseInt(JOptionPane.showInputDialog(null,str_numero));
+            metodo.validarNum(numero_a_adivinar);
+            numero_a_adivinar = metodo.getNum();
         }
-        int intentos = Integer.parseInt(JOptionPane.showInputDialog("INTRODUCE UN NÚMERO MÁXIMO DE INTENTOS"));
+        int intentos = Integer.parseInt(JOptionPane.showInputDialog(str_n_max_intentos));
         metodo.validarIntentos(intentos);
         intentos = metodo.getIntentos();
         int i = 0;
         do {
 
-            int adv_num = Integer.parseInt(JOptionPane.showInputDialog("ADIVINA EL NÚMERO TE QUEDAN " + (intentos-i)+ " INTENTOS"));
-          metodo.primeraPista(adv_num,i);
+            int adivinar_numero = Integer.parseInt(JOptionPane.showInputDialog( str_adv_num)+ (intentos-i)+ str_intentos);
+          metodo.primeraPista(adivinar_numero,i);
           if (metodo.getAdivinar()){
               i += intentos;
           }
@@ -33,7 +39,7 @@ public class Main {
 
         }while (i <= intentos);
         if(i== intentos){
-            JOptionPane.showMessageDialog(null,"OOOH!! HAS FALLADO, EL NÚMERO ERA: " + num);
+            JOptionPane.showMessageDialog(null,str_fallaste + numero_a_adivinar);
         }
 
     }
