f1536dcf23ec58494d5f0d46249df58d67a6cce2
Marcos F. Avendaño <marcosfavendano@gmail.com>
2022-12-12 08:43:14 +0100

    se actualizan todos los boletines

--- extras/untitled/src/Main.java
+++ extras/untitled/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int num = 5;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                num += 2;
+                
+            }
+        }
+
+        System.out.println(num);
     }
 }
